d166f12eff5aaa79ef3eb9d06578a3d9a6197b42
Marco Bacelo <marcobacelo90@gmail.com>
2019-10-31 13:51:56 -0300

    Update url of app_models

--- demandsProject/settings.py
+++ demandsProject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app_models'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'demandsProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["demandsProject/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

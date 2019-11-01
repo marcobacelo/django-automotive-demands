9903236560e5ad0abb5b0b26927fddf87b844258
Marco Bacelo <marcobacelo90@gmail.com>
2019-11-01 01:18:28 -0300

    Created a first relational model

--- app_models/models.py
+++ app_models/models.py
@@ -2,25 +2,37 @@ from django.db import models
 
 
 class Contact(models.Model):
-    email = models.CharField(max_length=100, primary_key=True)
+    email = models.EmailField(max_length=75, primary_key=True)
     address = models.CharField(max_length=100)
     celNumber = models.CharField(max_length=20)
 
+    class Meta:
+        db_table = 'contact'
+
 
 class Profile(models.Model):
-    profileName = models.CharField(max_length=10)
+    profileName = models.CharField(max_length=10, primary_key=True)
+
+    class Meta:
+        db_table = 'profile'
 
 
 class Demands(models.Model):
-    id = models.IntegerField(models.AutoField(primary_key=True))
-    description = models.TextField(max_length=300)
+    id = models.AutoField(primary_key=True)
+    description = models.TextField(max_length=500)
     receiver = models.CharField(max_length=15)
-    idPerson = models.ForeignKey('Person', on_delete=models.CASCADE, db_column='cpfPerson')
+    status = models.BooleanField(default=True, verbose_name='status')
+
+    class Meta:
+        db_table = 'demands'
 
 
 class Person(models.Model):
-    cpf = models.IntegerField(primary_key=True, db_column='cpf')
+    cpf = models.CharField(max_length=15, primary_key=True)
     name = models.CharField(max_length=100, db_column='name')
-    profile = models.ForeignKey('Profile', on_delete=models.CASCADE, db_column='profile')
-    contact = models.ForeignKey('Contact', on_delete=models.CASCADE, db_column='contact')
+    profile = models.ForeignKey('Profile', to_field="profileName", on_delete=models.CASCADE)
+    contact = models.OneToOneField('Contact', to_field='email', on_delete=models.CASCADE)
     demands = models.ManyToManyField(Demands, db_column='idDemands')
+
+    class Meta:
+        db_table = 'person'

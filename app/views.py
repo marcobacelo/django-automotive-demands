68622f73c407cccd6943b8ab9ccae3a58aa37a87
Marco Bacelo <marcobacelo90@gmail.com>
2019-11-09 19:39:55 -0300

    Add serializers to django api context

--- app/views.py
+++ app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Demands, UserDetails
+from .ModelSerializer import DemandsSerializer, UserSerializer, UserDetailSerializer
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+class DemandsListAPI(generics.ListCreateAPIView):
+    queryset = Demands.objects.all()
+    serializer_class = DemandsSerializer
+
+
+class UserDetailsList(generics.ListCreateAPIView):
+    queryset = UserDetails.objects.all()
+    serializer_class = UserDetailSerializer
+
+
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
